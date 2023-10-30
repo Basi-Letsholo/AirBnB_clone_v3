72d7641779522da842110ab6c3330504fbd3372c
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-10-30 09:10:50 -0700

    update index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """ Api route module """
 
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False, methods=["GET"])
@@ -10,4 +17,24 @@ def status():
     status_json = {
             "status": "OK"
             }
-    return (status_json)
+    return status_json
+
+
+@app_views.route('/stats', strict_slashes=False, methods=["GET"])
+def stats():
+    stats_json = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return stats_json
+
+
+@app_views.errorhandler(404)
+def page_not_found(error):
+    return {
+            "error": "Not found"
+            }, 404
